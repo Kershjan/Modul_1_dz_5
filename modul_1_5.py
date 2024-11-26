b323dd9472f07d0bb13b3f112e11f4f5d2d564d1
Kershjan <k.jump35@gmail.com>
2024-11-26 16:37:46 +0400

    Add files via upload

---
+++ modul_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 'Hello', 5, 2, True
+print(immutable_var)
+# immutable_var[0] = 'Hel'
+# Кортеж является неизменяемым
+mutable_list = [3,6,8, True]
+mutable_list[0] = 4
+mutable_list[3] = False
+print(mutable_list)
